1ffa5a96b6eea45fb3ae8e0f9ba0818604e4a8a4
Jimmy Chu <jimmychu0807@gmail.com>
2022-03-28 13:01:36 +0800

    updated

--- contracts/MyERC20PresetMinterPauser.sol
+++ contracts/MyERC20PresetMinterPauser.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract ERC20SwapToken is ERC20PresetMinterPauser{
+contract MyERC20PresetMinterPauser is ERC20PresetMinterPauser{
   constructor(string memory name, string memory symbol) ERC20PresetMinterPauser(name, symbol) {}
 }
--- deploy/02_deployOrderBook.ts
+++ deploy/02_deployOrderBook.ts
@@ -1,12 +1,16 @@
-import { ethers, deployments } from "hardhat"
+import { ethers } from "hardhat"
 import { HardhatRuntimeEnvironment } from 'hardhat/types'
 import { DeployFunction } from 'hardhat-deploy/types'
-import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol"
+
+const INITIAL_SUPPLY = ethers.constants.WeiPerEther.mul(1000)
+
+const isFulfilled = <T>(input: PromiseSettledResult<T>): input is PromiseFulfilledResult<T> =>
+  input.status === 'fulfilled'
 
 async function deploy(hre: HardhatRuntimeEnvironment) {
   const {
-    getNamedAccounts,
     deployments,
+    getNamedAccounts,
     network,
   } = hre
 
@@ -14,22 +18,44 @@ async function deploy(hre: HardhatRuntimeEnvironment) {
 
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
+  const { provider } = ethers
 
   // Deploy two extra ERC20 tokens if it is not a live network
   if (!network.live) {
-    ethers.getContract("ERC20PresetMinterPauser")
+    // const baseNonce = await provider.getTransactionCount(deployer)
+
+    // let nonceOffset = 0
+    // const getNonce = () => baseNonce + nonceOffset++
+
+    console.log('Deploy and mint DEFI1 and DEFI2...')
+
+    // Use `Promise.allSettled()` ?
+    await deploy('Defi1Deployment', {
+      contract: 'MyERC20PresetMinterPauser',
+      from: deployer,
+      args: ['Defi1 Token', 'DEFI1'],
+    })
+
+    await deploy('Defi2Deployment', {
+      contract: 'MyERC20PresetMinterPauser',
+      from: deployer,
+      args: ['Defi2 Token', 'DEFI2'],
+      nonce: 1
+    })
+
+    // mint the two tokens
+    // TODO: change to use `Promise.allSettled()` later
+    const defi1 = await ethers.getContract('Defi1Deployment', deployer)
+    const defi2 = await ethers.getContract('Defi2Deployment', deployer)
+    await defi1.mint(deployer, INITIAL_SUPPLY)
+    await defi2.mint(deployer, INITIAL_SUPPLY)
   }
 
-  const defi1 = await ethers.getContract('Defi1Deployment', deployer)
-  const defi2 = await ethers.getContract('Defi2Deployment', deployer)
-
-  await deploy('SimpleSwap', {
-    contract: 'SimpleSwap',
+  await deploy('OrderBook', {
     from: deployer,
-    args: [defi1.address, defi2.address, 1000],
     log: true
   })
 }
 
-deploy.tags = ['swap']
+deploy.tags = ['order_book']
 export default deploy
